e47dd756310474209bac8d6b294dba10801e3312
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-27 11:04:33 +0000

    Build Story Model

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var indexNumber = 0
+    
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left", choice2: "Take a right"),
+                   Story(title: "You see s tiger.", choice1: "Shout for help", choice2: "Play dead"),
+                   Story(title: "You find a treasure chest.", choice1: "Open it", choice2: "Check for traps")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = stories[indexNumber].title
+        choice1Button.setTitle(stories[indexNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[indexNumber].choice2, for: .normal)
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: Any) {
+    }
+    
 }
 
